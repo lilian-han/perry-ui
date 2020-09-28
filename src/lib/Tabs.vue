1066fac55b15eea16f7c154f2d8e3faf984af9f4
han <15162294433@163.com>
2020-09-28 17:34:37 +0800

    优化 tabs

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -3,7 +3,7 @@
   <div class="aha-tabs-nav" ref="container">
     <div class="aha-tabs-nav-item" :ref="
           (el) => {
-            if (el) navItems[index] = el
+            if (t === selected) selectedItem = el
           }
         " @click="select(t)" :class="{ selected: t === selected }" v-for="(t, index) in titles" :key="index">
       {{ t }}
@@ -20,8 +20,8 @@
 import {
   computed,
   onMounted,
-  onUpdated,
-  ref
+  ref,
+  watchEffect
 } from 'vue'
 import Tab from './Tab.vue'
 export default {
@@ -31,27 +31,25 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref < HTMLDivElement[] > ([])
+    const selectedItem = ref < HTMLDivElement > (null)
     const indicator = ref < HTMLDivElement > (null)
     const container = ref < HTMLDivElement > (null)
-    const x = () => {
-      const divs = navItems.value
-      const result = divs.filter((div) => div.classList.contains('selected'))[0]
-      const {
-        width
-      } = result.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const {
-        left: left1
-      } = container.value.getBoundingClientRect()
-      const {
-        left: left2
-      } = result.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
-    }
-    onMounted(x)
-    onUpdated(x)
+    onMounted(() => {
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -70,7 +68,7 @@ export default {
       context.emit('update:selected', title)
     }
     return {
-      navItems,
+      selectedItem,
       container,
       indicator,
       defaults,
