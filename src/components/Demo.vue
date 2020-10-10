46f43c7efd292f4785129ecdec538848e3081aa4
han <15162294433@163.com>
2020-10-10 11:44:06 +0800

    忽略dist+prism css 文件移到 css中引入

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="demo">
-    <h2>{{ component.__sourceCodeTitle }}</h2>
-    <div class="demo-component">
-      <component :is="component" />
-    </div>
-    <div class="demo-actions">
-      <Button @click="codeVisible = !codeVisible">查看代码</Button>
-    </div>
-    <div class="demo-code" v-if="codeVisible">
-      <pre class="language-css" v-html="html" />
-    </div>
+<div class="demo">
+  <h2>{{ component.__sourceCodeTitle }}</h2>
+  <div class="demo-component">
+    <component :is="component" />
   </div>
+  <div class="demo-actions">
+    <Button @click="codeVisible = !codeVisible">查看代码</Button>
+  </div>
+  <div class="demo-code" v-if="codeVisible">
+    <pre class="language-css" v-html="html" />
+  </div>
+</div>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
 import 'prismjs'
-import 'prismjs/themes/prism.css'
-import { computed, ref } from 'vue'
+import {
+  computed,
+  ref
+} from 'vue'
 const Prism = (window as any).Prism
 export default {
   props: {
@@ -44,26 +46,33 @@ export default {
 </script>
 
 <style lang="scss">
+@import 'prismjs/themes/prism.css';
 $border-color: #d9d9d9;
+
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
-  > h2 {
+
+  >h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
   }
+
   &-component {
     padding: 16px;
   }
+
   &-actions {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
   }
+
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-    > pre {
+
+    >pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
       margin: 0;
